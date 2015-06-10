7afa93d0c903a4447eedd0a280124d3110d4500b
Mike Lee <mikelee@CML-MBP-Diyi.local>
2015-06-09 21:49:08 -0700

    Add modernizr and jquery

---
+++ bubble/_js/game.js
@@ -0,0 +1,12 @@
+/**
+ * Game 
+ */
+
+// Global namespacing
+var BubbleShoot = window.BubbleShoot || {};
+// Game object within the namespace
+// Immediately Invoked Function Expression IIFE
+BubbleShoot.Game = (function($) {
+  var Game = function() {};
+  return Game;
+})(jQuery);
